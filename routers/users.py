d14f1f5d3f5805725e22199f9859ee04e1a9e94f
Jacob Lee <jlee93115@163.com>
2020-12-22 00:03:49 -0800

    Renamed files

--- routers/users.py
+++ routers/users.py
@@ -1,16 +1,15 @@
-from datetime import datetime, timedelta
 from paper_trader.models import Token
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 
 from paper_trader import crud
-from .utils import authenticate_user, create_access_token, get_current_user
+from paper_trader.core.authentication import authenticate_user, create_access_token, is_authenticated
 
 
 router = APIRouter()
 
-@router.post('/user/login')
+@router.post('/users/login')
 async def login(login_form: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(login_form.username, login_form.password)
     if not user:
@@ -19,13 +18,7 @@ async def login(login_form: OAuth2PasswordRequestForm = Depends()):
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=30)
     access_token = create_access_token(
         data={"sub": user['username']}
     )
     return {"access_token": access_token, "token_type": "bearer"}
-
-@router.get('/user/me')
-async def is_authenticated(token: Token):
-    user = get_current_user(token.access_token)
-    return user
