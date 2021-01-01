ff86ab3a459ad6f7e91762d7f935e10a9ab8ac45
Jacob Lee <jlee93115@163.com>
2020-12-31 23:58:25 -0800

    Removed unneeded imports and modified router names

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import requests
-import json
-import re
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from paper_trader.routers import securities, watchlist, users
+from paper_trader.routers import trades, watchlists, users, searches
 
 app = FastAPI()
 
 app.include_router(users.router)
-app.include_router(securities.router)
-app.include_router(watchlist.router)
+app.include_router(trades.router)
+app.include_router(watchlists.router)
+app.include_router(searches.router)
 
 
 app.add_middleware(
