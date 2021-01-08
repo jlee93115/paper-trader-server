19157dd8cee47d05dc4a472842503a22045ce820
Jacob Lee <jlee93115@163.com>
2021-01-07 23:41:39 -0800

    Add methods for generating and validating tokens

--- core/authentication.py
+++ core/authentication.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone, tzinfo
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import HTTPException, Depends, status
+from fastapi import HTTPException, Depends, status, Cookie
 
 from passlib.context import CryptContext
 from secrets import token_hex
@@ -35,12 +35,34 @@ def verify_password(entered_password, hash_password):
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    
-    # token is valid for 2 minutes
-    expire = datetime.utcnow() + timedelta(minutes=2)
 
+    # access token is valid for 2 minutes
+    expire = datetime.utcnow() + timedelta(minutes=2)
     to_encode.update({"exp": expire})
+    to_encode.update({'iat': datetime.utcnow()})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
+    return encoded_jwt
+
+
+def create_refresh_token(data: dict):
+    # Initialize datetime object
+    expiry = datetime.now()
+
+    to_encode = data.copy()
+    valid_token = crud.authentication.get_valid_refresh_token(data['sub'])
+    if valid_token is None:
+        # refresh token is valid for 30 days
+        expiry = datetime.now() + timedelta(days=30)
+    else:
+        expiry = valid_token['expiry'].replace(tzinfo=None)
+
+    to_encode.update({"exp": expiry})
+    to_encode.update({'iat': datetime.utcnow()})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
+
+    # implement refresh token rotation
+    crud.authentication.revoke_refresh_tokens(data['sub'])
+    crud.authentication.insert_refresh_token(data['sub'], encoded_jwt, expiry)
     return encoded_jwt
 
 
@@ -55,3 +77,28 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         return None
     user = crud.users.get_user(token_data.username)
     return user
+
+
+def validate_refresh_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+        token_data = TokenData(username=username)
+        token_status = crud.authentication.get_refresh_token_status(token_data.username, token)
+        if token_status is None:
+            return None
+
+        # Convert local time to timezone of expiry time and compare
+        expiry_timezone = token_status['expiry'].utcoffset()
+        tz = timezone(expiry_timezone)
+        current_date_aware = datetime.now(tz)
+
+        if(token_status['expired'] == False):
+            if (token_status['expiry'] > current_date_aware):
+                user = crud.users.get_user(token_data.username)
+                return user
+        crud.authentication.revoke_refresh_tokens(token_data.username)
+    except:
+        return None
