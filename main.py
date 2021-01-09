64676ae45a49dec6477c47657cd9c83e2517e35e
Jacob Lee <jlee93115@163.com>
2021-01-08 16:46:48 -0800

    Modify origins and imports

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from paper_trader.routers import trades, watchlists, users, searches
+from paper_trader import crud
 
 app = FastAPI()
 
@@ -10,11 +11,17 @@ app.include_router(trades.router)
 app.include_router(watchlists.router)
 app.include_router(searches.router)
 
+origins = [
+    'http://127.0.0.1:8080'
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+# Revoke all tokens on server startup
+crud.authentication.revoke_refresh_tokens('%')
